e8dc198faf73fc55ac4db1fdabc3785a7dbf90d1
dusha <dushasam26@gmail.com>
2024-11-18 19:32:11 +0100

    Add

--- laboratorium1/obj/Debug/net8.0/laboratorium1.AssemblyInfo.cs
+++ laboratorium1/obj/Debug/net8.0/laboratorium1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laboratorium1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+218a9eb1c1213a81d4559c7b573ba0de292d6db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eaff10b4e754201bc8c4437d2adcb2f666e3308")]
 [assembly: System.Reflection.AssemblyProductAttribute("laboratorium1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laboratorium1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
